6799b001162c8d64f666ed5eb4171f5192e2eb29
matthewross07 <mrvr@rams.colostate.edu>
2020-10-15 15:18:28 -0600

    Started linking WQP to COMID for NHD

---
+++ src/ecoli_mapper.R
@@ -0,0 +1,20 @@
+library(sf)
+library(tidyverse)
+library(mapview)
+library(nhdplusTools)
+
+ecoli_dat <- read_csv('data/Ecoli_sites.csv') %>%
+  select(-1) %>%
+  st_as_sf(.,coords=c('Long','Lat'), crs = 4269) %>%
+  st_transform(4263)
+
+
+
+
+ecoli_sub <- ecoli_dat %>%
+  sample_frac(0.01)
+
+
+comid <- discover_nhdplus_id(ecoli_sub[1,])
+
+comids
